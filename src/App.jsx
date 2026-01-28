e34d3148fe32442e9d6eda96b2622e5128d5dc31
Aminu Abdullahi <aminuzainawa08@gmail.com>
2026-01-28 23:40:27 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-50">
+      <h1 className="text-4xl font-bold text-blue-700">
+        Medynex Platform Ready
+      </h1>
+ 
+    </div>
+ 
+  )
+}
+
+export default App
